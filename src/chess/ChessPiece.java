3f1ac7d279fac89e05c83fc53452cfbc21a4435d
Vitor Vaz Maciel <vittorvaz13@gmail.com>
2020-03-26 02:27:39 -0300

    ALL UPDATE

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -4,16 +4,15 @@ import boardgame.Board;
 import boardgame.Piece;
 
 public class ChessPiece extends Piece {
-    
-    private Color color;
 
-    public ChessPiece(Board board, Color color) {
-        super(board);
-        this.color = color;
-    }
+	private Color color;
 
-    public Color getColor() {
-        return color;
-    }
-    
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
 }
